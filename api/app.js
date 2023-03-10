dbf562ee9f737b4bcb86433ed507765a7969dd87
Rodeghier <irodeghier@FLVS0.lan>
2023-03-10 15:13:25 -0500

    end of week

--- api/app.js
+++ api/app.js
@@ -12,6 +12,13 @@ const { Task } = require('./db/models/task.model');
 // Load Middleware
 app.use(bodyParser.json());
 
+// CORS HEADER MIDDLEWARE
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 /*  Route Handlers */
 
 /* List Routes */
@@ -41,11 +48,74 @@ app.post('/lists', (req, res) => {
 // PATCH /lists - updates an existing/specified list
 app.patch('/lists/:id', (req, res) => {
   // Updates the specified lists based off of the ID
+  List.findOneAndUpdate({ _id: req.params.id }, {
+    $set: req.body
+  }).then(()=> {
+    res.sendStatus(200);
+  })
 });
 
-app.delete('lists/:id', (req, res) => {
+app.delete('/lists/:id', (req, res) => {
   //Delete a specified list
+  List.findOneAndRemove({
+    _id: req.params.id
+  }).then((removedListDoc) => {
+    res.send(removedListDoc);
+  })
 });
+
+// Get /lists/:listId/tasks
+// Get all tasks in a specific list
+app.get('/lists/:listId/tasks', (req, res) => {
+  //return all tasks that belong to a specific list
+  Task.find({
+    _listId: req.params.listId
+  }).then((tasks)=> {
+    res.send(tasks)
+  })
+})
+
+app.get('/lists/:listId/tasks/:taskId', (req, res)=>{
+  Task.findOne({
+    _id: req.params.taskId,
+    _listId: req.params.listId
+  }).then((task)=>{
+    res.send(task)
+  })
+})
+
+app.post('/lists/:listId/tasks', (req,res)=>{
+  // Create a new task in a list specified by listId
+  let newTask = new Task({
+    title: req.body.title,
+    _listId: req.params.listId
+  })
+  newTask.save().then((newTaskDoc)=>{
+    res.send(newTaskDoc)
+  })
+})
+
+app.patch('/lists/:listId/tasks/:taskId', (req,res)=>{
+  Task.findOneAndUpdate({
+    _id: req.params.taskId,
+    _listId: req.params.listId
+  }, {
+    $set: req.body
+  }).then(()=>{
+    res.sendStatus(200)
+  })
+
+})
+
+
+app.delete('/lists/:listId/tasks/:taskId', (req,res)=>{
+  Task.findOneAndRemove({
+    _id: req.params.taskId,
+    _listId: req.params.listId
+  }).then((removedTaskDoc)=>{
+    res.send(removedTaskDoc)
+  })
+})
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000...');
